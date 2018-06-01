1f968a4f4bba77c31bf0c3ff5f81aaa1446a8c87
Masataka Hirano <gooogle4747@gmail.com>
2018-06-01 18:34:12 +0900

    Update Home to implement tablayout

--- app/src/main/java/com/example/alpacamusclemaintenance/MainActivity.kt
+++ app/src/main/java/com/example/alpacamusclemaintenance/MainActivity.kt
@@ -4,16 +4,18 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.example.alpacamusclemaintenance.adapters.PageAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
 
         val fragmentAdapter = PageAdapter(supportFragmentManager)
-        viewpager_main.adapter = fragmentAdapter
+        pager.adapter = fragmentAdapter
 
-        tabs_main.setupWithViewPager(viewpager_main)
+        tab.setupWithViewPager(pager)
     }
 }
