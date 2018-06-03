a4aed8c3faea9793730772e0da3e39ad3bde35c7
Masataka Hirano <gooogle4747@gmail.com>
2018-06-03 16:22:52 +0900

    Add WebView to show github repository

--- app/src/main/java/com/example/alpacamusclemaintenance/adapters/PageAdapter.kt
+++ app/src/main/java/com/example/alpacamusclemaintenance/adapters/PageAdapter.kt
@@ -6,6 +6,7 @@ import android.support.v4.app.FragmentPagerAdapter
 import com.example.alpacamusclemaintenance.fragments.FirstFragment
 import com.example.alpacamusclemaintenance.fragments.SecondFragment
 import com.example.alpacamusclemaintenance.fragments.ThirdFragment
+import com.example.alpacamusclemaintenance.fragments.WebViewFragment
 
 class PageAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
 
@@ -13,20 +14,22 @@ class PageAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
         return when (position) {
             0 -> FirstFragment()
             1 -> SecondFragment()
-            else -> ThirdFragment()
+            2 -> ThirdFragment()
+            else -> WebViewFragment()
         }
     }
 
     override fun getCount(): Int {
         // Show 3 total pages.
-        return 3
+        return 4
     }
 
     override fun getPageTitle(position: Int): CharSequence? {
         return when (position) {
             0 -> "First Tab"
             1 -> "Second Tab"
-            else -> "Third Tab"
+            2 -> "Third Tab"
+            else -> "Repo Tab"
         }
     }
 
