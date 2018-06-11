d447f596b82e382962770d545997962899f44bb1
Masataka Hirano <gooogle4747@gmail.com>
2018-06-12 01:16:02 +0900

    Update fragments to use synthetic to find view

--- app/src/main/java/com/example/alpacamusclemaintenance/fragments/ExerciseFragment.kt
+++ app/src/main/java/com/example/alpacamusclemaintenance/fragments/ExerciseFragment.kt
@@ -1,23 +1,22 @@
 package com.example.alpacamusclemaintenance.fragments
 
 import android.os.Bundle
-import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
-import android.support.v4.view.ViewPager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.alpacamusclemaintenance.R
 import com.example.alpacamusclemaintenance.adapters.ExercisePageAdapter
+import kotlinx.android.synthetic.main.fragment_exercise.view.*
 
 class ExerciseFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView = inflater.inflate(R.layout.fragment_exercise, container, false)
 
-        val pager = rootView.findViewById<ViewPager>(R.id.pager).apply {
+        val pager = rootView.pager.apply {
             adapter = ExercisePageAdapter(fragmentManager!!)
         }
-        rootView.findViewById<TabLayout>(R.id.tab).run {
+        rootView.tab.run {
             setupWithViewPager(pager)
         }
 
