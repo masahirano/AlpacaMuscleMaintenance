85ccd56b2be216e130f3d0127b99e08bdd64ca10
Masataka Hirano <gooogle4747@gmail.com>
2018-06-23 14:15:25 +0900

    Update PushUpFragment add save button and function

--- app/src/main/java/com/example/alpacamusclemaintenance/ui/PushUpFragment.kt
+++ app/src/main/java/com/example/alpacamusclemaintenance/ui/PushUpFragment.kt
@@ -8,14 +8,26 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.alpacamusclemaintenance.R
 import com.example.alpacamusclemaintenance.databinding.FragmentPushUpBinding
+import com.example.alpacamusclemaintenance.db.AppDatabase
+import com.example.alpacamusclemaintenance.db.entity.PushUp
+import kotlinx.coroutines.experimental.launch
 
 /**
  * A simple [Fragment] subclass.
  */
 class PushUpFragment : Fragment() {
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val binding: FragmentPushUpBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_push_up, container, false)
+
         binding.button.setOnClickListener { ++binding.count }
+        binding.finishButton.setOnClickListener {
+            launch {
+                val database = AppDatabase.getInstance(context!!)
+                database.pushUpDao().insert(PushUp(0, binding.count))
+                binding.count = 0
+            }
+        }
 
         return binding.root
     }
