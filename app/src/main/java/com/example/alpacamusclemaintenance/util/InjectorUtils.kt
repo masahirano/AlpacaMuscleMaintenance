db51f95434fd19e680b08b4e6aa29c413b2eb3e8
alpaca0984 <gooogle4747@gmail.com>
2019-04-29 23:16:18 +0900

    Clean up

--- app/src/main/java/com/example/alpacamusclemaintenance/util/InjectorUtils.kt
+++ app/src/main/java/com/example/alpacamusclemaintenance/util/InjectorUtils.kt
@@ -1,26 +1,13 @@
 package com.example.alpacamusclemaintenance.util
 
-import android.content.Context
-import com.example.alpacamusclemaintenance.db.AppDatabase
 import com.example.alpacamusclemaintenance.repository.FeedRepository
-import com.example.alpacamusclemaintenance.repository.PushUpRepository
 import com.example.alpacamusclemaintenance.viewmodel.FeedViewModelFactory
-import com.example.alpacamusclemaintenance.viewmodel.PushUpViewModelFactory
 
 object InjectorUtils {
-
-//    private fun getPushUpRepository(context: Context): PushUpRepository {
-//        return PushUpRepository.getInstance(AppDatabase.getInstance(context).pushUpDao())
-//    }
 
     private fun getFeedRepository(): FeedRepository {
         return FeedRepository.getInstance()
     }
-
-//    fun providePushUpViewModelFactory(context: Context): PushUpViewModelFactory {
-//        val repository = getPushUpRepository(context)
-//        return PushUpViewModelFactory(repository)
-//    }
 
     fun provideFeedViewModelFactory() : FeedViewModelFactory {
         val repository = getFeedRepository()
