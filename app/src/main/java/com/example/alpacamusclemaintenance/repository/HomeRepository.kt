a6ab5216b17a3c79de1d4a287f72a4e63cc30f04
alpaca0984 <gooogle4747@gmail.com>
2019-05-09 23:59:10 +0900

    Specified type explicitly

--- app/src/main/java/com/example/alpacamusclemaintenance/repository/HomeRepository.kt
+++ app/src/main/java/com/example/alpacamusclemaintenance/repository/HomeRepository.kt
@@ -8,8 +8,8 @@ import javax.inject.Inject
 class HomeRepository @Inject constructor() {
 
     fun getHome(): LiveData<Home> {
-        val data = MutableLiveData<Home>()
-        val words = arrayListOf(
+        val data: MutableLiveData<Home> = MutableLiveData()
+        val words: ArrayList<Home> = arrayListOf(
                 Home("Strength does not come from the physical capacity. It comes from an indomitable will.", "Ghandi"),
                 Home("The best way to predict the future is to create it.", "Abraham Lincoln"),
                 Home("Work hard in silence. Let success be your noise.", "Frank Ocean"),
