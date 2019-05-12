78a9ec2559f83b2792a95ead3496859dc17b5661
alpaca0984 <gooogle4747@gmail.com>
2019-05-12 16:22:13 +0900

    Refactor: Small changes on AppDatabase

--- app/src/main/java/com/example/alpacamusclemaintenance/db/AppDatabase.kt
+++ app/src/main/java/com/example/alpacamusclemaintenance/db/AppDatabase.kt
@@ -31,25 +31,25 @@ abstract class AppDatabase : RoomDatabase() {
         @Volatile
         private var instance: AppDatabase? = null
 
-        fun getInstance(context: Context): AppDatabase {
-            return instance ?: synchronized(this) {
-                instance ?: buildDatabase(context).also { instance = it }
-            }
-        }
+        fun getInstance(context: Context): AppDatabase =
+                instance ?: synchronized(this) {
+                    instance ?: buildDatabase(context).also { instance = it }
+                }
 
         // Create and pre-populate the database. See this article for more details:
         // https://medium.com/google-developers/7-pro-tips-for-room-fbadea4bfbd1#4785
-        private fun buildDatabase(context: Context): AppDatabase {
-            return Room.databaseBuilder(context, AppDatabase::class.java, DATABASE_NAME)
-                    .addCallback(object : RoomDatabase.Callback() {
-                        override fun onCreate(db: SupportSQLiteDatabase) {
-                            super.onCreate(db)
-                            val request = OneTimeWorkRequest.Builder(SeedDatabaseWorker::class.java).build()
-                            WorkManager.getInstance().enqueue(request)
-                        }
-                    })
-                    .fallbackToDestructiveMigration()
-                    .build()
-        }
+        private fun buildDatabase(context: Context): AppDatabase =
+                Room.databaseBuilder(context, AppDatabase::class.java, DATABASE_NAME)
+                        .addCallback(object : RoomDatabase.Callback() {
+                            override fun onCreate(db: SupportSQLiteDatabase) {
+                                super.onCreate(db)
+                                val request: OneTimeWorkRequest = OneTimeWorkRequest
+                                        .Builder(SeedDatabaseWorker::class.java)
+                                        .build()
+                                WorkManager.getInstance().enqueue(request)
+                            }
+                        })
+                        .fallbackToDestructiveMigration()
+                        .build()
     }
 }
