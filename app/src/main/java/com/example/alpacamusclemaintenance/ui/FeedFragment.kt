12b101d499e4912dc9e20b1d9af1c4cb60759c00
alpaca0984 <gooogle4747@gmail.com>
2019-12-24 21:45:21 +0900

    Refactor FeedAdapter

--- app/src/main/java/com/example/alpacamusclemaintenance/ui/FeedFragment.kt
+++ app/src/main/java/com/example/alpacamusclemaintenance/ui/FeedFragment.kt
@@ -1,25 +1,21 @@
 package com.example.alpacamusclemaintenance.ui
 
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.Nullable
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DividerItemDecoration
-import com.example.alpacamusclemaintenance.R
 import com.example.alpacamusclemaintenance.adapter.FeedAdapter
 import com.example.alpacamusclemaintenance.databinding.FragmentFeedBinding
 import com.example.alpacamusclemaintenance.di.Injectable
 import com.example.alpacamusclemaintenance.viewmodel.FeedViewModel
 import com.example.alpacamusclemaintenance.vo.Feed
 import javax.inject.Inject
-
 
 /**
  * A simple [Fragment] subclass.
@@ -40,14 +36,11 @@ class FeedFragment : Fragment(), Injectable {
     container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View? {
-    feedAdapter = FeedAdapter()
-
-    binding = DataBindingUtil.inflate(inflater, R.layout.fragment_feed, container, false)
-    binding.feed.let { feedView ->
-      feedView.adapter = feedAdapter
-      val dividerItemDecoration = DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
-      feedView.addItemDecoration(dividerItemDecoration)
-    }
+    binding = FragmentFeedBinding.inflate(
+      inflater,
+      container,
+      false
+    )
 
     return binding.root
   }
@@ -55,12 +48,23 @@ class FeedFragment : Fragment(), Injectable {
   override fun onActivityCreated(@Nullable savedInstanceState: Bundle?) {
     super.onActivityCreated(savedInstanceState)
 
-    feedViewModel = ViewModelProviders.of(this, viewModelFactory).get(FeedViewModel::class.java)
-    feedViewModel.feedObservable.observe(this, Observer<List<Feed>> { feeds ->
-      feeds?.let {
-        binding.progressBar.visibility = View.GONE
-        feedAdapter.setFeedList(it)
+    feedAdapter = FeedAdapter()
+    binding
+      .feed
+      .apply {
+        adapter = feedAdapter
+        addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
       }
-    })
+
+    feedViewModel =
+      ViewModelProviders
+        .of(this, viewModelFactory)
+        .get(FeedViewModel::class.java)
+    feedViewModel
+      .feedObservable
+      .observe(this, Observer<List<Feed>> { feeds ->
+        binding.progressBar.visibility = View.GONE
+        feedAdapter.setFeedList(feeds)
+      })
   }
 }
