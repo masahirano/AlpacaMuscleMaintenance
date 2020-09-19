b037196ed2156ee1beba91b65df20e06ce632275
alpaca0984 <gooogle4747@gmail.com>
2020-09-19 17:38:25 +0900

    Disable Flipper temporary for release build

--- app/src/main/java/com/example/alpacamusclemaintenance/AlpacaMuscleMaintenanceApp.kt
+++ app/src/main/java/com/example/alpacamusclemaintenance/AlpacaMuscleMaintenanceApp.kt
@@ -3,12 +3,12 @@
 package com.example.alpacamusclemaintenance
 
 import android.app.Application
-import com.facebook.flipper.android.AndroidFlipperClient
-import com.facebook.flipper.android.utils.FlipperUtils
-import com.facebook.flipper.plugins.databases.DatabasesFlipperPlugin
-import com.facebook.flipper.plugins.inspector.DescriptorMapping
-import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
-import com.facebook.soloader.SoLoader
+//import com.facebook.flipper.android.AndroidFlipperClient
+//import com.facebook.flipper.android.utils.FlipperUtils
+//import com.facebook.flipper.plugins.databases.DatabasesFlipperPlugin
+//import com.facebook.flipper.plugins.inspector.DescriptorMapping
+//import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+//import com.facebook.soloader.SoLoader
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
@@ -23,12 +23,12 @@ class AlpacaMuscleMaintenanceApp : Application() {
     }
 
     // https://fbflipper.com/docs/getting-started.html#setup-your-android-app
-    SoLoader.init(this, false)
-    if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
-      val client = AndroidFlipperClient.getInstance(this)
-      client.addPlugin(InspectorFlipperPlugin(this, DescriptorMapping.withDefaults()))
-      client.addPlugin(DatabasesFlipperPlugin(this))
-      client.start()
-    }
+//    SoLoader.init(this, false)
+//    if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
+//      val client = AndroidFlipperClient.getInstance(this)
+//      client.addPlugin(InspectorFlipperPlugin(this, DescriptorMapping.withDefaults()))
+//      client.addPlugin(DatabasesFlipperPlugin(this))
+//      client.start()
+//    }
   }
 }
